88c31ba5077b977d8c3069696af88677a0d605c7
POWCHU\Arwin Delasan <delasanarwin@gmail.com>
2025-06-07 17:58:12 +0800

    Add credentials JSON file to .gitignore

--- map/urls.py
+++
@@ -1,6 +0,0 @@
-from .views import nearby_restaurants
-from django.urls import path
-
-urlpatterns = [
-    path('search_places/', nearby_restaurants,name="nearby restaurants"),
-]
--- suggestions/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Suggestion, Prompt, Location
-
-admin.site.register(Suggestion)
-admin.site.register(Prompt)
-admin.site.register(Location)
-# Register your models here.
